6831df4f6120009ec8f7740104d3559122f309a5
Javier Damián Mendoza <hervarar@gmail.com>
2023-09-21 14:39:40 -0400

    add owl carousel

---
+++ public/js/OwlCarousel2-2.3.4/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#header-slider").owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,
+        navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>']
+    });
+});
